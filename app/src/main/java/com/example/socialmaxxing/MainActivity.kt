2836439402fb625948cd8cadd673af9ffbd1a6c2
Deniss Fjodorovs <3starblaze@protonmail.com>
2025-05-12 11:16:20 +0300

    Add singletons and permission obtain logic

--- app/src/main/java/com/example/socialmaxxing/MainActivity.kt
+++ app/src/main/java/com/example/socialmaxxing/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.socialmaxxing
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -14,24 +19,41 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import com.example.socialmaxxing.ui.theme.SocialmaxxingTheme
 
 class MainActivity : ComponentActivity() {
+    // NOTE: We have "arePermissionsReady" that does the thing
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         val activity = this
+        // TODO: Refactor this permission checking logic, it's rather repetitive
+        // probably a component with "permissionsReady" flag is a better alternative
+        var singletons: Singletons? = if (arePermissionsReady(activity)) getSingletons(activity) else null
 
         setContent {
             SocialmaxxingTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Box(modifier = Modifier.padding(innerPadding)) {
+                    Column (modifier = Modifier.padding(innerPadding)) {
+                        Text(text = if (singletons == null) "we need your permission" else "everything seems to be good")
                         // TODO: Don't show this when permissions are ok
                         RequestBluetoothButton(
                             onClick = {
                                 requestBluetoothPermissions(activity)
+                                if (arePermissionsReady(activity)) {
+                                    singletons = getSingletons(activity)
+                                    Toast.makeText(
+                                        activity,
+                                        "Everything ok with permissions!",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    Log.d("tmp_tag", "bluetooth permissions are ok!")
+                                } else {
+                                    Log.d("tmp_tag", "couldn't retrieve singletons, handle this gracefully!")
+                                }
                             }
                         )
                     }
@@ -39,6 +61,13 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+fun arePermissionsReady(activity: Activity): Boolean {
+    return (ContextCompat.checkSelfPermission(
+        activity,
+        Manifest.permission.BLUETOOTH_CONNECT,
+    ) == PackageManager.PERMISSION_GRANTED)
 }
 
 @Composable
---
+++ app/src/main/java/com/example/socialmaxxing/Singletons.kt
@@ -0,0 +1,36 @@
+package com.example.socialmaxxing
+
+import android.Manifest
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.BluetoothLeScanner
+import android.content.Intent
+import androidx.annotation.RequiresPermission
+
+data class Singletons (
+    val activity: Activity,
+    val bluetoothManager: BluetoothManager,
+    val bluetoothAdapter: BluetoothAdapter,
+    val bluetoothLeScanner: BluetoothLeScanner,
+)
+
+@RequiresPermission(Manifest.permission.BLUETOOTH_CONNECT)
+fun getSingletons(activity: Activity): Singletons {
+    val context = activity.applicationContext
+    val bluetoothManager: BluetoothManager = context.getSystemService(BluetoothManager::class.java)
+    val bluetoothAdapter: BluetoothAdapter = bluetoothManager.adapter
+
+    if (bluetoothAdapter.isEnabled == false) {
+        val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+
+        activity.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BLUETOOTH)
+    }
+
+    return Singletons(
+        activity = activity,
+        bluetoothManager = bluetoothManager,
+        bluetoothAdapter = bluetoothAdapter,
+        bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner,
+    )
+}
